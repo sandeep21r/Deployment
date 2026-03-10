e39c3074b76aa0785a646109f10453f3bf792d4b
sandeep.s <sandeep.s@dpworld.com>
2026-03-10 20:07:42 +0530

    Dd

--- core/config.py
+++ core/config.py
@@ -2,7 +2,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str = ""
-   
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
